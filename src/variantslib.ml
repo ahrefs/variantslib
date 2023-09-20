6fab234f3faaabe2ac0ddd38dcf7f0035ad8ccc6
Rusty Key <rusty.key@ahrefs.com>
2023-09-20 14:14:39 +0000

    vendor base.variantslib

--- src/variantslib.ml
+++ src/variantslib.ml
@@ -1,1 +1,8 @@
-include Base.Exported_for_specific_uses.Variantslib
+module Variant = struct
+  type 'constructor t =
+  { name : string
+  ; (* the position of the constructor in the type definition, starting from 0 *)
+    rank : int
+  ; constructor : 'constructor
+  }
+end
